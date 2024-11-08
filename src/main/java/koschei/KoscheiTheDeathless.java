f898edaf6483ea9e20033c6b82c708b66d3268da
Boidar <mity.grood1@gmail.com>
2024-11-08 21:18:48 +0300

    first commit

---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,26 @@
+package koschei;
+
+import koschei.models.Egg6;
+import koschei.models.Ocean1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+    private Egg6 egg;
+
+    public String getRulesByDeth() {
+        return "На море " + ocean.toString();
+    }
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
+}
